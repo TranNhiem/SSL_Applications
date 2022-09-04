1a43fdd8c3c16fdb1f821f70a41dd774caf3235b
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2022-09-04 16:18:08 +0800

    Complete_image_inpainting_app

--- image_inpaiting_app.py
+++ image_inpaiting_app.py
@@ -1,21 +1,22 @@
 
 import gradio as gr
 import PIL
+import os
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import torch
 from torch import autocast
 import torchcsprng as csprng
-from stable_diffusion_inpainting import StableDiffusionInpaintingPipeline
-
+from stable_diffusion_inpainting import StableDiffusionInpaintingPipeline_
+from glob import glob
 # from huggingface_hub import notebook_login
 # notebook_login() ## Using for Colab or jupyter notebook
 # Running your python script using
 # huggingface-cli login
 
 # Using the image inpainting pipeline
-pipeimg = StableDiffusionInpaintingPipeline.from_pretrained(
+pipeimg = StableDiffusionInpaintingPipeline_.from_pretrained(
     "CompVis/stable-diffusion-v1-4", revision="fp16",
     torch_dtype=torch.float16,
     use_auth_token=True,
@@ -74,61 +75,78 @@ def mask_processes(mask):
 
 
 generator = csprng.create_random_device_generator('/dev/urandom')
+example_dir = "/home/rick/code_spaces/SSL_Applications/Bird_images"
+# example_dir="Bird_images"
 
 
 def infer(prompt, img, samples_num, steps_num, scale, option):
 
-    if option == "Replace selection":
+    if option == "Mask Sketch Area":
         mask = 1 - mask_processes(img['mask'])
     else:
         mask = mask_processes(img['mask'])
     img = image_preprocess(img['image'])
+    print(prompt)
 
     # Generate image for the masking area with prompt
     with autocast("cuda"):
-        images = pipeimg([prompt]*samples_num, img, mask,
-                         num_inference_steps=steps_num, guidance_scale=scale,  generator=generator)
-
+        images = pipeimg([prompt]*samples_num, init_image=img, mask_image=mask,
+                         num_inference_steps=steps_num, guidance_scale=scale, generator=generator)["sample"]  # generator=generator
     return images
 
 
 with block as demo:
     gr.Markdown(
-        "<h1><center> Image Inpainting App </center></h1>arbitrary resolutions should be 'working'")
+        "<h1><center> Image Inpainting App </center></h1> Different image resolutions should be 'working'")
+
+    gr.Markdown(
+        "<h3><center> Illustration How to use this App </center></h3> https://drive.google.com/file/d/11JLQ9Slc7Udyw8Mcs0QWzEVy8kQotqrf/view?usp=sharing ")
+
     with gr.Group():
         with gr.Box():
             with gr.Row().style(mobile_collapse=False, equal_height=True):
-                text = gr.Textbox(label="Inpainting with Your prompt", show_label=False, max_lines=10).style(
+                text = gr.Textbox(label="Inpainting with Your prompt", placeholder="Enter In-painting expected object here...", show_label=True, max_lines=1).style(
                     border=(True, False, True, True),
                     rounded=(True, False, False, True),
-                    container=False,
-                )
-                btn = gr.Button("Run").style(margin=False,
-                                             rounded=(False, True, True, False),)
+                    container=False,)
+                btn = gr.Button("Run").style(
+                    margin=True, rounded=(True, True, True, True),)
 
-            with gr.Row().style(mobile_collapse=False, equal_height=True):
-                samples_num = gr.Slider(
-                    label="Number of samples", min=1, max=4, value=1, step=1,)  # show_label=False
-                steps_num = gr.Slider(
-                    label="Generatio of steps", min=2, max=499, value=80, step=1,)  # show_label=False
-                scale = gr.Slider(label="Guidance scale", min=0.0,
-                                  max=30, value=7.5, step=0.1,)  # show_label=False
+        with gr.Row().style(mobile_collapse=False, equal_height=True):
+            samples_num = gr.Slider(label="Number of Generated Image",
+                                    minimum=1, maximum=10, value=1, step=1,)  # show_label=False
+            steps_num = gr.Slider(
+                label="Generatio of steps", minimum=2, maximum=499, value=80, step=1,)  # show_label=False
+            scale = gr.Slider(label="Guidance scale", minimum=0.0,
+                              maximum=30, value=7.5, step=0.1,)  # show_label=False
 
-            with gr.Row().style(mobile_collapse=False, equal_height=True):
-                option = gr.Radio(label="Inpainting option", choices=[
-                                  "Replace selection", "Inpaint outside selection"], show_label=False)
+        with gr.Row().style(mobile_collapse=False, equal_height=True):
+            option = gr.Radio(label="Inpainting Area", default="Mask Sketch Area", choices=[
+                "Mask Sketch Area", "Background Area"], show_label=True)
 
-            image = gr.Image(source='upload', tool="sketch",
-                             label="Input image", type="numpy")
+        image = gr.Image(source="upload", tool="sketch",
+                         label="Input image", type="numpy")
+        gallery = gr.Gallery(label="Generated images",
+                             show_label=True).style(grid=[2], height="auto")
+        text.submit(fn=infer, inputs=[
+                    text, image, samples_num, steps_num, scale, option], outputs=gallery)
 
-            gallery = gr.Gallery(label="Generated images", show_label=False).style(
-                grid=[2], height="auto"
-            )
+        btn.click(fn=infer, inputs=[text, image, samples_num,
+                  steps_num, scale, option], outputs=gallery)
 
-            text.submit(infer, inputs=[
-                        text, image, samples_num, steps_num, scale, option], outputs=gallery)
-            btn.click(infer, inputs=[
-                      text, image, samples_num, steps_num, scale, option], outputs=gallery)
+        # with gr.Row("20%"):
+        #      gr.Markdown("#### (Illustration of Using This App):")
+        #      gr.Image(f"{example_dir}/Rotating_earth_(large).gif", shape=(224, 224))
 
-demo.launch(server_name="0.0.0.0",  server_port=5555,
-            share=False, enable_queue=True, )  # debug=True)
+        # Using example images provide
+        # with gr.Row("20%"):
+        #     gr.Markdown("#### (Image Example):")
+        #     #ims_uri = [f"{example_dir}/{x}" for x in os.listdir(example_dir)]##
+        #     ims_uri=f"{example_dir}/343785.jpg"
+        #     # ims_uri = [ex for ex in ims_uri]
+
+        #     image = gr.Image(tool="sketch",label="Input image", type="numpy", value=str(ims_uri))#value=f"{example_dir}/343785.jpg") #source='upload',  # value=os.path.join(os.path.dirname(__file__),".jpg"
+        #     gr.Examples(fn=infer, examples=[os.path.join(os.path.dirname(__file__)),image], inputs=[text, image, samples_num,steps_num, scale, option], outputs=gallery, cache_examples=True)
+
+demo.launch(server_name="140.115.53.102",  server_port=5555,
+            share=True, enable_queue=True, )  # debug=True)
